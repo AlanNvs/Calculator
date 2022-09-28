5ef24842b3d8d459739031a7f2f81f875c0c1332
shftdv <114028830+shftdv@users.noreply.github.com>
2022-09-27 22:32:53 -0300

    Fixed operate function wasn't returning anything

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 function add(a, b) {
   return a + b;
 }
@@ -14,16 +15,12 @@ function divide(a, b) {
 function operate(a, b, operator) {
   switch (operator) {
     case '+':
-      add(a, b);
-      break;
+      return add(a, b);
     case '-':
-      subtract(a, b);
-      break;
+      return subtract(a, b);
     case '*':
-      multiply(a, b);
-      break;
+      return multiply(a, b);
     case '/':
-      divide(a, b);
-      break;
+      return divide(a, b);
   }
 }
